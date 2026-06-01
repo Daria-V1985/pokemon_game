a274d8f2c8198163c9df28f870ace848f9d62358
Daria <darjana29@gmail.com>
2026-06-01 12:01:00 +0300

    feat: верстка блока с инвентарем

--- pokemon/src/components/Cell.vue
+++ pokemon/src/components/Cell.vue
@@ -1,31 +1,47 @@
 <template>
-  <div :class="['grid-cell', { active: props.isActive }]">
-    <img v-if="props.itemSrc" :src="props.itemSrc" :alt="props.itemAlt" class="grid-cell__item" />
+  <div :class="['grid-cell', cellModClass]">
+    <img 
+      v-if="props.itemSrc" 
+      :src="props.itemSrc" 
+      :alt="props.itemAlt || 'item'" 
+      class="grid-cell__item" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue';
 
 interface Cells {
-  isActive: boolean;
   itemSrc?: string; 
   itemAlt?: string; 
+  itemType?: 'berry' | 'pokeball' | null;
 }
 
-const props = defineProps<Cells>();
+const props = withDefaults(defineProps<Cells>(), {
+  itemSrc: '',
+  itemAlt: '',
+  itemType: null
+});
+
+const cellModClass = computed(() => {
+  if (!props.itemType) return 'grid-cell--empty';
+  return `grid-cell--${props.itemType}`;
+});
+
 </script>
 
 <style scoped>
 .grid-cell {
-  background-color: #e5e7eb8c;
+  background-color: #efefef;
+  opacity: 0.3;
   border-radius: 8px;
   aspect-ratio: 1 / 1;
   display: flex;
   justify-content: center;
   align-items: center;
-  &.active {
-    border: 2px solid #e5e7eb;
-  }
+  transition: all 0.2s ease;
+  border: 1px solid transparent;
+  cursor: pointer;
   &__item {
     max-width: 80%;
     max-height: 80%;
--- pokemon/src/components/Inventory.vue
+++ pokemon/src/components/Inventory.vue
@@ -3,12 +3,37 @@
     <div class="inventory__container">
       <div class="inventory__body">
         <div class="inventory__title">Inventory</div>
+        <div class="inventory__grid">
+          <Cell 
+            v-for="(_, index) in 50"
+            :key="index"
+            :index="index"
+            :itemSrc="''"
+            :itemAlt="''"
+            :itemType="null"
+          />
+          <div class="inventory__coins-bar coins-bar">
+            <div class="coins-bar__balance">
+              <span class="coins-bar__icon">
+                <img src="../assets/image/poke_coin.png" alt="Монеты">
+              </span>
+              <span class="coins-bar__sum">{{ userStore.money }}</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+//import { ref } from 'vue';
+import { useUserStore } from '@/stores/useUserStore';
+import Cell from './Cell.vue';
+
+const userStore = useUserStore();
+
+</script>
 
 <style lang="scss" scoped>
 @import "../assets/scss/_variables.scss";
@@ -19,9 +44,42 @@
     padding: 16px;
     border-radius: 16px;
     box-shadow: 0px 0px 16px rgba(58, 58, 58, 0.1);
+    max-width: 320px;
+    box-sizing: border-box;
   }
   &__title {
     @include design-text;
+    margin-bottom: 16px;
+  }
+  &__grid {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 8px;
+  }
+}
+
+.coins-bar {
+  grid-column: 1 / -1;
+  border: 3px solid #3b63bf;
+  border-radius: 4px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  &__balance {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+  &__icon {
+    width: 32px;
+    height: 32px;
+  }
+  &__sum {
+    @include design-text;
+    line-height: 29px;
+    padding-left: 4px;
   }
 }
 </style>
